d682b1f976a2a42f5985fc0fb5f24c1d39864ef9
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-11 17:09:10 +0900

    [TEST] CustomUserDetailService에 UserDetail 잘 반환되는 지 테스트 코드 추가

--- backend/springboot/src/test/java/com/example/testlocal/module/user/application/service/CustomUserDetailServiceTest.java
+++ backend/springboot/src/test/java/com/example/testlocal/module/user/application/service/CustomUserDetailServiceTest.java
@@ -1,4 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example.testlocal.module.user.application.service;
+
+import com.example.testlocal.module.user.domain.VO.UserVO;
+import com.example.testlocal.module.user.domain.entity.User;
+import com.example.testlocal.module.user.domain.repository.UserRepository2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebAppConfiguration
+@SpringBootTest
 class CustomUserDetailServiceTest {
-  
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    private UserRepository2 userRepository;
+
+    @DisplayName("UserDetail 반환하는 비교를 한다.")
+    @Test
+    void UserDetail_반환() {
+        User user = User.builder().id(1).email("tovbskvb@daum.net").name("박태순").password("1234567").studentNumber("17011526").build();
+        userRepository.save(user);
+        UserDetails userDetails = customUserDetailService.loadUserByUsername("17011526");
+        assertThat(userDetails.getUsername()).isEqualTo("박태순");
+    }
 }
