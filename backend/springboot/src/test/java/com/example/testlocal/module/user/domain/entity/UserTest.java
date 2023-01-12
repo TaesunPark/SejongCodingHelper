066c03b3a3325f322e83a89dbf7fd8de46e375f2
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-12 14:04:45 +0900

    [TEST] User Entity RoleType 테스트 코드 추가

--- backend/springboot/src/test/java/com/example/testlocal/module/user/domain/entity/UserTest.java
+++ backend/springboot/src/test/java/com/example/testlocal/module/user/domain/entity/UserTest.java
@@ -1,5 +1,6 @@
 package com.example.testlocal.module.user.domain.entity;
 
+import com.example.testlocal.config.RoleType;
 import com.example.testlocal.module.user.domain.repository.UserRepository2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,7 +22,7 @@ class UserTest {
 
     @BeforeEach
     public void init(){
-        user1 = User.builder().email("tovbskvb@daum.net").studentNumber("17011526").password("1234").name("손민기").build();
+        user1 = User.builder().email("tovbskvb@daum.net").studentNumber("17011526").password("1234").name("손민기").roleType(RoleType.ADMIN).build();
     }
 
     @DisplayName("유저 생성, 테스트")
@@ -34,6 +35,7 @@ class UserTest {
         assertThat(user1.getRoleType()).isEqualTo(user2.getRoleType());
         assertThat(user1.getStudentNumber()).isEqualTo(user2.getStudentNumber());
         assertThat(user1.getEmail()).isEqualTo("tovbskvb@daum.net");
+        assertThat(user1.getRoleType()).isEqualTo(RoleType.ADMIN);
     }
 
     @DisplayName("유저 업데이트, 테스트")
@@ -43,6 +45,8 @@ class UserTest {
         user1.setEmail("tovbskvb1@daum.net");
         user1.setName("발민기");
         user1.setStudentNumber("17011568");
+        user1.setRoleType(RoleType.USER);
+
         user2 = userRepository.save(user1);
         assertThat(user1.getName()).isEqualTo(user2.getName());
         assertThat(user1.getId()).isEqualTo(user2.getId());
@@ -50,6 +54,7 @@ class UserTest {
         assertThat(user1.getRoleType()).isEqualTo(user2.getRoleType());
         assertThat(user1.getStudentNumber()).isEqualTo(user2.getStudentNumber());
         assertThat(user1.getEmail()).isEqualTo("tovbskvb1@daum.net");
+        assertThat(user1.getRoleType()).isEqualTo(RoleType.USER);
     }
 
     @DisplayName("유저 삭제")
