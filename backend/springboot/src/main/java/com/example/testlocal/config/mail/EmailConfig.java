944811d5ed2cef571c81810732dd611603060959
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-15 22:07:44 +0900

    [Test] EmailTestConfig 추가

--- backend/springboot/src/main/java/com/example/testlocal/config/mail/EmailConfig.java
+++ backend/springboot/src/main/java/com/example/testlocal/config/mail/EmailConfig.java
@@ -1,13 +1,54 @@
-package com.example.testlocal.config;
+package com.example.testlocal.config.mail;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Slf4j
 @Configuration
 public class EmailConfig {
+    @Value("${spring.mail.host}")
+    String host;
 
+    @Value("${spring.mail.username}")
+    String username;
+
+    @Value("${spring.mail.password}")
+    String password;
+
+    @Bean(name = "MyMailService")
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setUsername(username);
+        javaMailSender.setHost(host);
+        javaMailSender.setPassword(password);
+        javaMailSender.setPort(465);
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.protocol", "smtps");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+        return properties;
+    }
 }
+//spring.mail.host=smtp.naver.com
+//spring.mail.port=465
+//# ?? naver ???? ??
+//spring.mail.username=sjcodinghelper@naver.com
+//spring.mail.password=sjcoding123!
+//#???? ?? prop?? ssl? ??, ?? ? ????
+//spring.mail.properties.smtp.auth=true
+//spring.mail.properties.smtp.ssl.enable=true
+//spring.mail.properties.smtp.ssl.trust=smtp.naver.com
+//spring.mail.protocol=smtps
