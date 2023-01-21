8fbce3783b51f953bc056c08d733ba97b553e1f1
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-21 19:06:49 +0900

    [Test] EmailServiceTest 추가

--- backend/springboot/src/test/java/com/example/testlocal/module/user/application/service/EmailServiceTest.java
+++ backend/springboot/src/test/java/com/example/testlocal/module/user/application/service/EmailServiceTest.java
@@ -2,15 +2,23 @@ package com.example.testlocal.module.user.application.service;
 
 
 import com.example.testlocal.config.mail.EmailTestConfig;
+import com.example.testlocal.module.user.application.dto.SendEmailRequest;
+import com.example.testlocal.module.user.application.dto.request.EmailCodeRequest;
+import com.example.testlocal.module.user.application.dto.response.EmailCodeResponse;
+import com.example.testlocal.module.user.application.dto.response.SendEmailResponse;
+import com.example.testlocal.module.user.domain.repository.EmailCertificationDao;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -19,41 +27,39 @@ import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith({SpringExtension.class})
-@Import({EmailTestConfig.class})
+@WebAppConfiguration
+@SpringBootTest
 class EmailServiceTest {
 
-    @InjectMocks
+    @Autowired
     private EmailService emailService;
 
     @Autowired
-    private JavaMailSender MyMailService;
+    private EmailCertificationDao emailCertificationDao;
 
-    @DisplayName("이메일 보내는 테스트 결과 값은 메일 확인")
-    @Test
-    public void sendEmail() throws MessagingException {
-        String email = "tovbskvb@sju.ac.kr";
+    private SendEmailRequest sendEmailRequest;
+    private EmailCodeRequest emailCodeRequest;
 
-        String content = "test";
-
-        // 메일 보내기
-        MimeMessage message = MyMailService.createMimeMessage();
-
-        MimeMessageHelper h = new MimeMessageHelper(message,"UTF-8");
-
-        h.setFrom("sjcodinghelper@naver.com");
-        h.setTo(email);
-        h.setSubject("Sejong Coding Helper 회원가입 인증 메일");
-        h.setText(content);
-        h.setSentDate(new Date());
-        MyMailService.send(h.getMimeMessage());
+    @BeforeEach
+    public void setValue(){
+        sendEmailRequest = new SendEmailRequest();
+        emailCodeRequest = new EmailCodeRequest();
+        sendEmailRequest.setEmail("tovbskvb");
+        emailCodeRequest.setEmail("tovbskvb");
+        emailCodeRequest.setAuthCode("12345");
     }
 
-    @DisplayName("랜덤 값 확인 테스트")
     @Test
-    public void random() throws MessagingException {
-        String value1 = emailService.randomCode();
-        assertThat(value1).isNotSameAs(emailService.randomCode());
+    void sendSejongEmail() throws MessagingException {
+        SendEmailResponse sendEmailResponse = emailService.sendSejongEmail(sendEmailRequest, null);
+        assertThat(sendEmailResponse.getEmail()).isEqualTo(sendEmailRequest.getEmail());
     }
 
+    @Test
+    void checkEmailAuthCode() {
+        emailCertificationDao.createCodeCertification(emailCodeRequest.getEmail(), "12345");
+        EmailCodeResponse emailCodeResponse = emailService.checkEmailAuthCode(emailCodeRequest, null);
+        assertThat(emailCodeResponse.getAuthCode()).isEqualTo("12345");
+        assertThat(emailCodeResponse.getIsSuccessful()).isEqualTo(true);
+    }
 }
