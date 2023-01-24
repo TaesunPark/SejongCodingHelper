4ed274041bc45706a4c818f6c25038b7033e8271
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-24 20:55:33 +0900

    [ADD] NotFoundException 클래스 추가

--- backend/springboot/src/main/java/com/example/testlocal/core/exception/NotFoundException.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/exception/NotFoundException.java
@@ -1,2 +1,12 @@
-package com.example.testlocal.core.exception;public class NotFoundException {
+package com.example.testlocal.core.exception;
+
+public class NotFoundException extends BusinessException{
+
+    public NotFoundException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+
+    public NotFoundException(String message){
+        super(message, ErrorCode.NOT_FOUND_EXCEPTION);
+    }
 }
