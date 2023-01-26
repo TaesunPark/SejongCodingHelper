0f944913e14948b135ca2e21f0ce4315f6a63686
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-27 03:04:54 +0900

    [TEST] 유저 체크 서비스에 대한 테스트 추가

--- backend/springboot/src/test/java/com/example/testlocal/module/user/application/service/impl/UserCheckServiceTest.java
+++ backend/springboot/src/test/java/com/example/testlocal/module/user/application/service/impl/UserCheckServiceTest.java
@@ -1,4 +1,51 @@
-import static org.junit.jupiter.api.Assertions.*;
-class UserCheckServiceImplTest {
-  
+package com.example.testlocal.module.user.application.service.impl;
+
+import com.example.testlocal.config.RoleType;
+import com.example.testlocal.module.user.application.dto.UserDto;
+import com.example.testlocal.module.user.application.dto.response.EmailCheckResponse;
+import com.example.testlocal.module.user.application.service.UserCheckService;
+import com.example.testlocal.module.user.domain.entity.User;
+import com.example.testlocal.module.user.domain.repository.UserRepository2;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebAppConfiguration
+@SpringBootTest
+class UserCheckServiceTest {
+
+    @Autowired
+    private UserCheckServiceImpl userCheckService;
+
+    @Autowired
+    private UserRepository2 userRepository;
+
+    @BeforeEach
+    public void init(){
+        userRepository.deleteAll();
+    }
+
+    @DisplayName("중복 이메일 테스트 성공")
+    @Test
+    public void isOverlapEmail(){
+        EmailCheckResponse emailCheckResponse = userCheckService.isOverlapEmail("tovbskvb@daum.net");
+        assertThat(emailCheckResponse.getIsDuplicated()).isEqualTo(true);
+    }
+
+    @DisplayName("중복 이메일 테스트 실패")
+    @Test
+    public void isOverlapEmail_FAIL(){
+        UserDto userDTO = new UserDto("17011526", "1234", "박태순", "tovbskvb@sju.ac.kr");
+        User user = User.builder().email(userDTO.getEmail()).roleType(RoleType.USER).name(userDTO.getName()).studentNumber(userDTO.getStudentNumber()).password(userDTO.getPassword()).build();
+        userRepository.save(user);
+        EmailCheckResponse emailCheckResponse = userCheckService.isOverlapEmail("tovbskvb@sju.ac.kr");
+        assertThat(emailCheckResponse.getIsDuplicated()).isEqualTo(false);
+    }
 }
