c17077135e9ac19097358c24fe0f6ac28f797684
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-28 05:21:16 +0900

    [MODIFY] EmailCodeResponse 클래스 변경 isSuccessFul 삭제, 빌더 적용 해제

--- backend/springboot/src/main/java/com/example/testlocal/module/user/application/dto/response/EmailCodeResponse.java
+++ backend/springboot/src/main/java/com/example/testlocal/module/user/application/dto/response/EmailCodeResponse.java
@@ -4,17 +4,10 @@ import lombok.*;
 
 @ToString
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder(access = AccessLevel.PRIVATE)
 public class EmailCodeResponse {
     private String authCode;
-    private Boolean isSuccessful;
 
-    public static EmailCodeResponse of(String authCode, Boolean isSuccessful) {
-        return EmailCodeResponse.builder()
-                .isSuccessful(isSuccessful)
-                .authCode(authCode)
-                .build();
+    public EmailCodeResponse(String authCode){
+        this.authCode = authCode;
     }
 }
