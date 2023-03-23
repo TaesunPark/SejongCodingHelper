e9a4a769d9c634fcfd3148463d99853226101f57
tovbskvb@daum.net <tovbskvb@daum.net>
2023-03-24 01:25:04 +0900

    [ADD] 로그아웃 시 RefreshToken 삭제

--- backend/springboot/src/main/java/com/example/testlocal/module/user/domain/repository/RefreshTokenRepository.java
+++ backend/springboot/src/main/java/com/example/testlocal/module/user/domain/repository/RefreshTokenRepository.java
@@ -1,2 +1,15 @@
-package com.example.testlocal.module.user.domain.repository;public class RefreshTokenRepository {
+package com.example.testlocal.module.user.domain.repository;
+
+import com.example.testlocal.module.user.domain.entity.RefreshToken;
+import com.example.testlocal.module.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    Integer deleteByUser(User user);
 }
