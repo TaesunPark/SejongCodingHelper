965f73f5ab7ccf0ed8be1fdfc508dd36dfbd38f4
tovbskvb@daum.net <tovbskvb@daum.net>
2023-03-24 01:05:02 +0900

    [MODIFY] 확장 클래스 변경 GenericFilterBean -> OncePerRequestFilter

--- backend/springboot/src/main/java/com/example/testlocal/core/security/jwt/JwtAuthenticationFilter.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/security/jwt/JwtAuthenticationFilter.java
@@ -1,25 +1,29 @@
-package com.example.testlocal.core.security.Jwt;
+package com.example.testlocal.core.security.jwt;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// GenericFilterBean 와 차이점
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         // 헤더에서 JWT 를 받아옵니다.
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        System.out.println(token);
         // 유효한 토큰인지 확인합니다.
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
