76ded4345ba7ab5f64a53a7294abaf595fef81ea
tovbskvb@daum.net <tovbskvb@daum.net>
2023-03-23 22:10:14 +0900

    [ADD] TokenRefreshRequest dto 추가

---
+++ backend/springboot/src/main/java/com/example/testlocal/core/security/jwt/dto/TokenRefreshRequest.java
@@ -0,0 +1,16 @@
+package com.example.testlocal.core.security.jwt.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class TokenRefreshRequest {
+    @NotBlank
+    private String refreshToken;
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
