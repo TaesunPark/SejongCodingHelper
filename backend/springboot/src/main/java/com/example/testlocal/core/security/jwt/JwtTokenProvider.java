21d07711ce7f0f12f4188716cb61f0254de9362f
tovbskvb@daum.net <tovbskvb@daum.net>
2023-03-24 01:11:10 +0900

    [REFACTOR] 토큰 생성 의존성 삭제

--- backend/springboot/src/main/java/com/example/testlocal/core/security/jwt/JwtTokenProvider.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/security/jwt/JwtTokenProvider.java
@@ -1,46 +1,34 @@
-package com.example.testlocal.core.security.Jwt;
+package com.example.testlocal.core.security.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
-    private String secretKey = "byukbyubyubyukkk";
-    private long tokenValidTime = 30 * 60 * 1000L;
+    private static final String PREFIX_BEARER = "Bearer ";
+    @Value("${sjc.app.jwtSecret}")
+    private String secretKey;
     private final UserDetailsService userDetailsService;
-
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
-    }
-
-    public String createToken(String userPk, Long time) {
-
-        tokenValidTime = time * 60 * 1000L;
-
-        Claims claims = Jwts.claims().setSubject(userPk);
-        Date now = new Date();
-
-        return Jwts.builder()
-                .setClaims(claims) // 정보 저장
-                .setIssuedAt(now) // 토큰 발행 시간 정보
-                .setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
-                .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과
-                // signature 에 들어갈 secret값 세팅
-                .compact();
     }
 
     // JWT 토큰에서 인증 정보 조회
@@ -54,9 +42,14 @@ public class JwtTokenProvider {
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 
-    // Request의 Header에서 token 값을 가져옵니다. "X-AUTH-TOKEN" : "TOKEN값'
     public String resolveToken(HttpServletRequest request) {
-        return request.getHeader("X-AUTH-TOKEN");
+        String bearerToken = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(PREFIX_BEARER)) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+        //return request.getHeader("X-AUTH-TOKEN");
     }
 
     // 토큰의 유효성 + 만료일자 확인
