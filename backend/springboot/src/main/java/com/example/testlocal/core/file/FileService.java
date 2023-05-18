ff82cf641f781e9983d6daa9b2a62eea62579951
tovbskvb@daum.net <tovbskvb@daum.net>
2023-05-18 15:15:23 +0900

    [ADD] CompilerService에서 File 삭제, 추가 해주는 기능들을 분리 및 FileService에 책임 전가

--- backend/springboot/src/main/java/com/example/testlocal/core/file/FileService.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/file/FileService.java
@@ -1,2 +1,31 @@
-package com.example.testlocal.core.file;public class FileService {
+package com.example.testlocal.core.file;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class FileService {
+
+    // 파일 존재하면 삭제
+    public void deleteFileIfExists(String path) throws IOException {
+        Files.deleteIfExists(Path.of(path));
+    }
+
+    // 파일 생성
+    public void createFile(String text, String path){
+        try (OutputStream outputStream = Files.newOutputStream(Path.of(path))) {
+            byte[] bytes = text.getBytes();
+            outputStream.write(bytes);
+        } catch (IOException e) {
+            System.out.println(path);
+            System.out.println(text);
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+
 }
