380c72e35dd5fda490375d5a3f620bfa41371a7d
tovbskvb@daum.net <tovbskvb@daum.net>
2023-05-18 15:14:03 +0900

    [ADD] 외부 프로세스 실행 파일 경로 enum으로 관리

--- backend/springboot/src/main/java/com/example/testlocal/core/file/FilePathEnum.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/file/FilePathEnum.java
@@ -1,10 +1,16 @@
 package com.example.testlocal.core.file;
 
 public enum FilePathEnum {
-    FILE1("path/to/file1.txt"),
-    FILE2("path/to/file2.txt"),
-    FILE3("path/to/file3.txt");
-    
+    C_FILE_PATH("/Users/taesunpark/home/c/gcc/test4.c"),
+    C_INPUT_TEXT_FILE_PATH("/Users/taesunpark/home/c/gcc/test.txt"),
+    C_EXECUTION_FILE_PATH("/Users/taesunpark/home/c/gcc/test"),
+    C_EXECUTION_PATH("/Users/taesunpark/home/c/gcc/test"),
+    PYTHON_FILE_PATH("/Users/taesunpark/home/python3/test.py"),
+    PYTHON_INPUT_TEXT_FILE_PATH("/Users/taesunpark/home/python3/test.txt"),
+    PYTHON_FILENAME("test.py"),
+    PYTHON_EXECUTION_PATH("/Users/taesunpark/home/python3/test.py")
+    ;
+
     private final String path;
     
     private FilePathEnum(String path) {
