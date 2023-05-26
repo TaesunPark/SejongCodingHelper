b86f194f4f1256ce2fa9528b4ce9fbf4b377d38c
tovbskvb@daum.net <tovbskvb@daum.net>
2023-05-26 15:25:37 +0900

    [REFACTOR] jwt 패키지에서 밖으로 빼줌

--- backend/springboot/src/main/java/com/example/testlocal/core/security/CustomUserDetailService.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/security/CustomUserDetailService.java
@@ -1,6 +1,7 @@
-package com.example.testlocal.core.security.jwt.service;
+package com.example.testlocal.core.security;
 
-import com.example.testlocal.core.security.jwt.CustomUserDetails;
+import com.example.testlocal.core.exception.login.InvalidCredentialsException;
+import com.example.testlocal.module.user.domain.entity.User;
 import com.example.testlocal.module.user.domain.repository.UserRepository2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,16 +9,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository2 userRepository;
     @Override
     public UserDetails loadUserByUsername(String studentNumber) throws UsernameNotFoundException {
-        return new CustomUserDetails(userRepository.findByStudentNumber(studentNumber)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException("사용자를 찾을 수 없습니다.")
-                )
-        );
+
+        Optional<User> user = userRepository.findByStudentNumber(studentNumber);
+
+        if (user.isEmpty()){
+            throw new InvalidCredentialsException();
+        }
+
+        return new CustomUserDetails(user.get());
     }
 }
--- backend/springboot/src/main/java/com/example/testlocal/core/security/CustomUserDetails.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/security/CustomUserDetails.java
@@ -1,4 +1,4 @@
-package com.example.testlocal.core.security.jwt;
+package com.example.testlocal.core.security;
 
 import com.example.testlocal.module.user.domain.entity.User;
 import org.springframework.security.core.GrantedAuthority;
