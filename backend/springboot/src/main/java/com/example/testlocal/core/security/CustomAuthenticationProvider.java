96447a2cd62fac750022b2043dd15f49036ea3c2
taesunpark <tovbskvb@daum.net>
2023-06-02 22:47:02 +0900

    [ADD] CustomAuthenticationProvider 비밀번호 5회 틀릴 시 예외 발생 처리, 로그인 했을 때 비밀번호 틀린 횟수 초기화 처리 추가

--- backend/springboot/src/main/java/com/example/testlocal/core/security/CustomAuthenticationProvider.java
+++ backend/springboot/src/main/java/com/example/testlocal/core/security/CustomAuthenticationProvider.java
@@ -1,7 +1,11 @@
 package com.example.testlocal.core.security;
 
+import com.example.testlocal.core.exception.login.InvalidCredentialsException;
+import com.example.testlocal.module.user.domain.repository.UserRepository2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -11,13 +15,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
     private CustomUserDetailService userDetailsService;
+
+    @Autowired
+    private UserRepository2 userRepository;
     private PasswordEncoder passwordEncoder;
 
     public CustomAuthenticationProvider(CustomUserDetailService userDetailsService, PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
     }
-
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -26,14 +32,20 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
         CustomUserDetails user = (CustomUserDetails) userDetailsService.loadUserByUsername(username);
 
-        // 사용자가 없을 때
-        if (user == null) {
-            throw new BadCredentialsException("username is not found. username=" + username);
+        // 초과 횟수가 5회가 넘으면
+        if (user.getPasswordCount() >= 5){
+            throw new InvalidCredentialsException("비밀번호 5회 초과로"+username+"(계정)이 잠겼습니다.");
         }
+
         // 아이디는 있는데 비밀번호가 다를때
         // lock 작업, 5회 이상 틀리면 lock
         if (!this.passwordEncoder.matches(password, user.getPassword())) {
-            throw new BadCredentialsException("password is not matched");
+            userRepository.save(user.addPasswordCount());
+            throw new InvalidCredentialsException();
+        }
+
+        if (user.getPasswordCount() != 0) {
+            userRepository.save(user.initPasswordCount());
         }
 
         return new CustomAuthenticationToken(username, user.getEmail(), user.getAuthorities());
